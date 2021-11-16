a489951c376c4180dcf9f5a37d762896aab14b1c
Minzino <gurumcider@gmail.com>
2021-11-16 18:32:53 +0900

    이차원배열수정

--- src/gugudan_class/Gugudan.java
+++ src/gugudan_class/Gugudan.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Gugudan {
 	public static int[][] calculate(int times1, int times2) {
-		int[][] result = new int[times1][times2];
-		for (int i = 0; i < times1; i++) {
+		int[][] result = new int[times1-1][times2];
+		for (int i = 0; i < (times1-1); i++) {
 			for (int j = 0; j < times2; j++) {
-				result[i][j] = (times1 + i) * (j + 1);
+				result[i][j] = (i + 2) * (j + 1);
 			}
 		}
 		return result;
 	}
 
 	public static void print(int[][] result) {
-		for (int i = 0; i < (result.length-1); i++) {
+		for (int i = 0; i < result.length; i++) {
 			for (int j = 0; j < result[i].length; j++) {
-				System.out.println((i+2) + " X " + (j + 1) + " = " +result[i][j]);
+				System.out.println( (i + 2) + " X " + (j + 1) + " = " +result[i][j]);
 			}
 		}
 
--- src/gugudan_class/GugudanMain.java
+++ src/gugudan_class/GugudanMain.java
@@ -1,7 +1,11 @@
 package gugudan_class;
 
 import java.util.Scanner;
-
+/*
+ * 사용자가 입력한 값에 따라 크기가 다른 구구단을 계산해 출력한다.
+ * 예를 들어 사용자가 "8,7"과 같은 문자열을 입력하면 팔칠단을 구현한다. 
+ * 팔칠단은 2 * 1 ... 2 * 7, 3 * 1 ... 3 * 7, ... , 8 * 1 ... 8 * 7 까지 구현하는 것을 의미한다.
+*/
 public class GugudanMain {
 	public static void main(String[] args) {
 		
