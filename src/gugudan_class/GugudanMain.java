7c81bd5be3c38c57f4f90974c7f2366175b07d5b
Minzino <gurumcider@gmail.com>
2021-11-17 20:57:32 +0900

    예시용 커밋

--- src/gugudan_class/GugudanMain.java
+++ src/gugudan_class/GugudanMain.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
  * 예를 들어 사용자가 "8,7"과 같은 문자열을 입력하면 팔칠단을 구현한다. 
  * 팔칠단은 2 * 1 ... 2 * 7, 3 * 1 ... 3 * 7, ... , 8 * 1 ... 8 * 7 까지 구현하는 것을 의미한다.
 */
+//예시
 public class GugudanMain {
 	public static void main(String[] args) {
 		
@@ -15,6 +16,5 @@ public class GugudanMain {
 		int second = Integer.parseInt(number[1]);
 		int[][] result = Gugudan.calculate(first,second);
 		Gugudan.print(result);
-
 	}
 }
