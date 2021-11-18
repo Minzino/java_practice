0229e360e65040c4b5221f519b1637ac9eb74491
Minzino <gurumcider@gmail.com>
2021-11-18 14:51:40 +0900

    Update calendar main()

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -2,7 +2,13 @@ package calendar;
 
 public class Calendar {
 	public static void main(String[] args) {
-		System.out.println("Hello, Calander");
-		
+		StringBuilder sb = new StringBuilder();
+		sb.append("  일  월  화  수  목  금  토  일\n");
+		sb.append(" --------------------------\n");
+		sb.append("  1   2   3   4   5   6   7\n");
+		sb.append("  8   9  10  11  12  13  14\n");
+		sb.append(" 15  16  17  18  19  20  21\n");
+		sb.append(" 22  23  24  25  26  27  28");
+		System.out.println(sb);
 	}
  }
